16314d32fa8bbf187bfdffd757e0e0c67a3bea9c
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-01-02 13:21:05 -0300

    adicionado exercicio 10

---
+++ src/ex10_mediaHarmonica/Main.java
@@ -0,0 +1,36 @@
+package ex10_mediaHarmonica;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o tamanho da lista: ");
+        int tamanhoLista = entrada.nextInt();
+
+        double[] numeros = new double[tamanhoLista];
+
+        System.out.println("Digite os números da lista:");
+        for (int i = 0; i < tamanhoLista; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = entrada.nextDouble();
+        }
+
+        double mediaHarmonica = calcularMediaHarmonica(numeros);
+        System.out.println("A média harmônica é: " + mediaHarmonica);
+
+        entrada.close();
+    }
+    private static double calcularMediaHarmonica(double[] numeros) {
+        double somaReciprocos = 0;
+
+        for (double numero : numeros) {
+            somaReciprocos += 1 / numero;
+        }
+        double mediaHarmonica = numeros.length / somaReciprocos;
+
+        return mediaHarmonica;
+    }
+
+}
