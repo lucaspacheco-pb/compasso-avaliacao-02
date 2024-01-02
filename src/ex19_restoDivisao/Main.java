6c60c996dfd772fd97173cdbfeca4ff6bd146431
Lucas Pacheco <lucas.pacheco.pb@compasso.com.br>
2024-01-02 02:24:51 -0300

    adicionando exercicio 19

--- src/ex19_restoDivisao/Main.java
+++ src/ex19_restoDivisao/Main.java
@@ -1,10 +1,32 @@
 package ex19_restoDivisao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+    	Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        int dividendo, divisor;
 
+        do {
+            System.out.print("Informe um número inteiro (Dividendo) maior que zero: ");
+            dividendo = scanner.nextInt();
+
+            if(dividendo <= 0) System.out.println("Número inválido. Tente novamente...");
+        } while(dividendo <= 0);
+
+        do {
+            System.out.print("Informe um número inteiro (Divisor) maior que zero e menor ou igual ao número anterior: ");
+            divisor = scanner.nextInt();
+
+            if(divisor <= 0 || divisor >= dividendo) System.out.println("Número inválido. Tente novamente...");
+        } while(divisor <= 0 || divisor >= dividendo);
+
+        System.out.println("O resto da divisão de " + dividendo + " por " + divisor + " é igual a " + dividendo % divisor);
+
+        scanner.close();
+    }
 }
