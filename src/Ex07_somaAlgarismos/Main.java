69016f5b72a5df82e417b034738d1e20060e903f
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2024-01-02 00:13:44 -0300

    adicionado exercicio 07

---
+++ src/Ex07_somaAlgarismos/Main.java
@@ -0,0 +1,46 @@
+package Ex07_somaAlgarismos;
+// Crie um programa que calcule a soma dos dígitos de um número usando um loop.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+
+        do {
+            String str = entrada.nextLine();
+
+            if (isNumeroInteiro(str)) {
+                int numero = Integer.parseInt(str);
+
+                int soma = 0;
+                int resto;
+
+                while (numero != 0) {
+                    resto = numero % 10; // Obtém o último dígito
+                    numero /= 10;
+                    soma += resto;
+                }
+
+                System.out.println("A soma dos dígitos é: " + soma);
+                break;
+            } else {
+                System.out.println("Por favor, insira um número inteiro válido.");
+            }
+
+        }while(true);
+
+        entrada.close();
+    }
+
+    private static boolean isNumeroInteiro(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (RuntimeException e) { // quando a conversão da string não ocorre para inteiro
+            return false;
+        }
+    }
+}
